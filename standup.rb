d0fb134fb95fa1434f1d213e97e0000137b3bda8
Ole Michaelis <Ole.Michaelis@googlemail.com>
2014-02-04 18:24:24 +0100

    figure around with ruby forking action

--- standup.rb
+++ standup.rb
@@ -3,13 +3,9 @@ songs = File.open(File.join(File.dirname(__FILE__), "Standupfile")).readlines.ea
 random_song = songs[rand(songs.size)]
 
 play_time = ARGV[0].to_i || 10
-xvfb = Process.fork do
-    exec "Xvfb :99 -ac"
-end
+xvfb = spawn "Xvfb :99 -ac"
+browser = spawn "firefox --display=:99 #{random_song}"
 
-browser = Process.fork do
-    exec "firefox --display=:99 #{random_song}"
-end
 sleep play_time
 Process.kill "TERM", browser
 Process.kill "TERM", xvfb
