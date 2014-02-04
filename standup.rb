e9fb63588c48a25858280b3ede5913f17d51f3f2
Ole Michaelis <Ole.Michaelis@googlemail.com>
2014-02-04 18:27:49 +0100

    refactor all the shit out of this

--- standup.rb
+++ standup.rb
@@ -1,11 +1,11 @@
 #!/usr/bin/ruby
 songs = File.open(File.join(File.dirname(__FILE__), "Standupfile")).readlines.each{|song|song.chomp! }
-random_song = songs[rand(songs.size)]
 
 play_time = ARGV[0].to_i || 10
 xvfb = spawn "Xvfb :99 -ac"
-browser = spawn "firefox --display=:99 #{random_song}"
+browser = spawn "firefox --display=:99 songs.sample"
 
 sleep play_time
 Process.kill "TERM", browser
+sleep 5 #wait for browser to quit
 Process.kill "TERM", xvfb
